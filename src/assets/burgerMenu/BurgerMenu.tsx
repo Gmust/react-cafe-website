62750b27929c27831179d248a0790664ef1d37de
Gmust <4orgood@gmail.com>
2022-10-17 17:50:29 +0200

    Start working on burger menu and created useWindowDimensions hook

---
+++ src/assets/burgerMenu/BurgerMenu.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './burgerMenu.css';
+
+const BurgerMenu = () => {
+    return (
+        <div>
+            Burger
+        </div>
+    );
+};
+
+export default BurgerMenu;
